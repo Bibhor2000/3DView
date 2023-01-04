fec3aac7b1b38ec02855cbe56640e4e90945eb7c
Bibhor2000 <daxter740@gmail.com>
2023-01-03 20:40:02 -0500

    react state in insert js is able to store the urls to the firebase storage files, currently facing an issue with setting state

--- src/components/Insert.js
+++ src/components/Insert.js
@@ -19,7 +19,8 @@ const firebaseConfig = initializeApp ({
     storageBucket: "view-5a6a6.appspot.com",
     messagingSenderId: "916014327766",
     appId: "1:916014327766:web:07cd0a3b03c72616e9e677",
-    measurementId: "G-SW50LTRLHN"
+    measurementId: "G-SW50LTRLHN",
+    rules_version: "2"
   });
 
 function Insert () {
@@ -27,62 +28,116 @@ function Insert () {
   const storage = getStorage(firebaseConfig);
   const database = getDatabase(firebaseConfig);
   const [modelRef, setModelRef] = useState();
-  const [modelList, setModelList] = useState();
-  const [model, setModel] = useState();
+  const [models, setModels] = useState([]);
+  const [allItems, setAllItems] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [display, setDisplay] = useState();
+
+  let modelArray = [];
 
   useEffect(() => {
     //Model Display Setup
-    let models = [];
-    let listRef = ref(storage, 'gs://view-5a6a6.appspot.com/uid')
-
+    let listRef = ref(storage, 'gs://view-5a6a6.appspot.com/')
+    let urlList = []
+    let buttonList = []
+    //Model Mapping Setup
     listAll(listRef)
     .then((res) => {
+      // console.log(res)
+      setAllItems(res.items)
+      console.log(res.items, 'this res items')
+      console.log(allItems, 'allItems array')
       res.items.forEach((itemRef) => {
         //All items 
-      })
+        // console.log(itemRef)
+        getDownloadURL(ref(storage, itemRef))
+        .then((url) => {
+          console.log(url, 'model url')
+          urlList.push(url)
+          // setModels(url, ...models)
+          
     })
-
-    console.log(listRef)
-
-    getDownloadURL(ref(storage, 'gs://view-5a6a6.appspot.com/harimau.glb'))
-    .then((url) => {
-      setModelRef(url)
     
-      models.map((m) => {
-      
-        return (
-            <div><model-viewer
-              className='card'
-              src={`${modelRef}`}
-              camera-controls
-              auto-rotate
-              autoplay='true'
-            ></model-viewer></div>
-      )
-      });
-
-      setModelList(models)
-    })
     .catch((error) => {
     });
+      })
+      console.log(urlList, 'url List')
+      setModels(urlList)
+      setDisplay(models[0])
+    })
+    // console.log(listRef)
+    // setLoaded(true)
+
+  //   let shipLink = getDownloadURL(ref(storage, 'gs://view-5a6a6.appspot.com/egx.glb'))
+  // .then((url) => {
+  //   // console.log(url)
+  //   setModelRef(url)
+  // })
+  // .catch((error) => {
+  // });
+
   }, [])
+
+  
+
+  console.log(models, 'this is model state')
+  console.log(modelArray, 'this is the model array')
+  // const viewModel = models.map((url) => {
+  //   console.log(url)
+  //   return (
+  //   <div><model-viewer
+  //   className='card'
+  //   src={url}
+  //   camera-controls
+  //   auto-rotate
+  //   autoplay='true'
+  //   ></model-viewer></div>
+  // )
+  // })
 
   // console.log(modelList)
   // console.log(modelRef)
   // console.log(model)
-
+  console.log(modelArray[0])
   return (
     <div>
-      <div>
-        <input></input>
-        <input></input>
-        <input></input>
-        <button></button>
-      </div>
-      <div className='card'>{modelList}</div>
+      {/* { models.length &&  */}
+      <><div>
+          <input></input>
+          <input></input>
+          <input></input>
+          <button></button>
+          {/* {models.map((url) => {
+    <div><model-viewer
+    className='card'
+    src={url}
+    camera-controls
+    modelCach
+    auto-rotate
+    autoplay='true'
+    ></model-viewer></div>
+  })} */}
+        {/* {viewModel} */}
+        </div>
+        {models.map((model, index) => {
+          return(
+            <button onClick={(event)=>{event.preventDefault()
+            setDisplay(model)}}>{index}</button>
+          )
+        })}
+        <div className='card'>
+        <model-viewer
+        className='card'
+        src={display}
+        camera-controls
+        auto-rotate
+        autoplay='true'
+    ></model-viewer></div>
+    </>
+{/* } */}
     </div>
   )
-
+    
 }
 
 export default Insert;
